23c46c33b20c12c44b3c9104cc0b9202f47d1150
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-11-25 10:57:57 +0100

    live coding pt2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,7 @@
+import MainMenu from "../components/MainMenu";
+
+
+
 export default function About() {
     return (
         <>
@@ -5,12 +9,7 @@ export default function About() {
                 <div className="logo">
                     LOGO
                 </div>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/books">Books</a>
-                    <a href="/about">About</a>
-                    <a href="/contacts">Contacts</a>
-                </nav>
+                <MainMenu />
             </header>
 
             <main>
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,3 +1,5 @@
+import MainMenu from "../components/MainMenu";
+
 export default function Books() {
     return (
         <>
@@ -5,12 +7,7 @@ export default function Books() {
                 <div className="logo">
                     LOGO
                 </div>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/books">Books</a>
-                    <a href="/about">About</a>
-                    <a href="/contacts">Contacts</a>
-                </nav>
+                <MainMenu />
             </header>
 
             <main>
